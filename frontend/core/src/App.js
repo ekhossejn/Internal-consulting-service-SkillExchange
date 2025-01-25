6b5f0827eab30fa6d20c7170b1dc3600c14abade
evpytoks <kuznetdaria@gmail.com>
2025-01-25 17:56:18 +0300

    init frontend

---
+++ frontend/core/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+  <>
+  <div>
+    <Navbar />
+    <Container >
+      <h1>Skill Exchange</h1>
+    </Container>
+    <Footer/>
+  </div>
+  </>
+  )
+}
