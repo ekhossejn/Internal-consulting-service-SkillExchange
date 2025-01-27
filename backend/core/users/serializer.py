fbb8940f7f600e502e1f574b29c0794e92e0d7be
ekhossejn <ekhossejn@gmail.com>
2025-01-27 22:20:52 +0300

    add jwt, login, get profile and users

--- backend/core/users/serializer.py
+++ backend/core/users/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Request, Document
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class RequestsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,39 @@ class DocumentsSerializer(serializers.ModelSerializer):
     class Meta:
         model=Document
         fields='__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model=User
+        fields=['id', '_id', 'username', 'email', 'name', 'isAdmin']
+
+    def get_name(self, obj):
+        first_name = obj.first_name
+        last_name = obj.last_name
+        name = first_name + ' ' + last_name
+        if name == ' ':
+            name = 'Set your name'
+        return name
+    
+    def get__id(self, obj):
+        return obj.id
+    
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+
+class UserSerializerWithToken(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model=User
+        fields=['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+    
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+    
--- backend/core/users/urls.py
+++ backend/core/users/urls.py
@@ -2,8 +2,10 @@ from users import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.printName, name="printName"),
     path('requests/', views.getRequests, name="getRequests"),
     path('request/<str:_id>', views.getRequest, name="getRequest"),
-    path('documents/', views.getDocuments, name="getDocuments")
+    path('documents/', views.getDocuments, name="getDocuments"),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('', views.getProfile, name="getProfile"),
+    path('users/', views.getUsers, name="getUsers")
 ]
