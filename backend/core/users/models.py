905056076ad671ee2db8efa813433ec86b520ff3
ekhossejn <ekhossejn@gmail.com>
2025-02-03 01:34:59 +0300

    add models

--- backend/core/users/models.py
+++ backend/core/users/models.py
@@ -5,12 +5,15 @@ from authentication.models import CustomUser
 class Document(models.Model):
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
-    pass
+    def __str__(self):
+        return (str)(self.owner)
 
 class Company(models.Model):
-    domain = models.CharField(null=False, blank=False)
+    domain = models.CharField(max_length=255, null=False, blank=False)
     name = models.CharField(max_length=20, null=False, blank=False)
     description = models.CharField(max_length=200, null=False, blank=True)
+    def __str__(self):
+        return self.name
 
 class Request(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
@@ -20,15 +23,18 @@ class Request(models.Model):
     createdAt = models.TimeField(auto_now_add=True)
     isActive = models.BooleanField(default=True)
     respondedUsers = models.ManyToManyField(CustomUser, related_name='requests', blank=True)
-
     def __str__(self):
         return self.name
 
 class Skill(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
+    def __str__(self):
+        return self.name
 
 class Review(models.Model):
-    reviewer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    reviewee = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reviews_as_reviewer')
+    reviewee = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reviews_as_reviewee')
     text =  models.CharField(max_length=500, null=False, blank=False)
     rating = models.IntegerField(null=False, default=0)
+    def __str__(self):
+        return self.rating
