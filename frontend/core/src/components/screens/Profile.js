3b9772acc8819d800669d74167aa221e3261e7ea
evpytoks <kuznetdaria@gmail.com>
2025-02-04 00:27:47 +0300

    minor fixes

--- frontend/core/src/components/screens/Profile.js
+++ frontend/core/src/components/screens/Profile.js
@@ -10,37 +10,12 @@ function Profile() {
   const accessToken = userInfo?.access;
   const [documents, setDocuments] = useState([]);
 
-  useEffect(() => {
-    if (!userInfo) {
-      navigate("/login");
-      return;
-    }
 
-    async function fetchdocuments() {
-      const { data } = await axios.get("/profile/documents/", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      setDocuments(data);
-    }
-    fetchdocuments();
-  }, []);
 
   return (
     <Container>
       <br />
       <h1>Мой профиль</h1>
-      <Row>
-        {documents.map((document) => (
-          <Col key={document.id} sm={12} md={6} lg={4} xl={3}>
-            <Card className="my-3 p-3 rounded">
-              <img src={document.image} alt="" />
-            </Card>
-            <br />
-          </Col>
-        ))}
-      </Row>
     </Container>
   )
 }
