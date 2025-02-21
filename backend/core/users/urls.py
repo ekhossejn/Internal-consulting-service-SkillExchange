3f24dde6c1f879d0a7fccbdeadb84f6c02d16b5a
ekhossejn <ekhossejn@gmail.com>
2025-02-22 02:32:11 +0300

    add profile/request/id/active/get/ and profile/request/id/active/change/

--- backend/core/users/urls.py
+++ backend/core/users/urls.py
@@ -2,8 +2,13 @@ from users import views
 from django.urls import path
 
 urlpatterns = [
+    path('image/get/', views.imageGet),
+
+    path('request/<str:_id>/active/get/', views.requestActiveGet),
+    path('request/<str:_id>/active/change/', views.requestActiveChange),
     path('request/create/', views.requestCreate),
     path('request/get/<str:_id>/', views.requestGet),
+    path('request/<str:_id>/emails/get/', views.requestEmailsGet),
     path('requests/get/', views.requestsGet),
 
     path('documents/get/', views.documentsGet),
