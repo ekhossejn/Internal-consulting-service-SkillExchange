7240b771140e9431d17e91bc1e1fc86c859ae4ad
evpytoks <kuznetdaria@gmail.com>
2025-04-05 12:29:25 +0300

    review size

--- frontend/core/src/components/Review.js
+++ frontend/core/src/components/Review.js
@@ -3,16 +3,18 @@ import { Card, Row, Col } from "react-bootstrap";
 import Rating from "./Rating";
 
 const options = {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
+  year: "numeric",
+  month: "long",
+  day: "numeric",
 };
 
 function Review({ review }) {
   return (
     <Card
       className="my-3 p-3 rounded"
-      style={{ width: "100%", backgroundColor: "var(--bs-light)" }}
+      style={{ width: "100%", backgroundColor: "var(--bs-light)",             maxHeight: "20vh",
+        height: "20vh",
+        overflow: "auto",}}
     >
       <Card.Header style={{ borderBottom: "none" }}>
         <h3>{review.reviewer_name}</h3>
@@ -20,11 +22,13 @@ function Review({ review }) {
           style={{
             display: "flex",
             alignItems: "center",
-            justifyContent: "space-between",
+            justifyContent: "space-between"
           }}
         >
           <Rating value={review.rating} color="var(--bs-warning)" />
-          <span style={{ marginLeft: "8px"}}>{new Date(review.createdAt).toLocaleString('ru-RU', options)}</span>
+          <span style={{ marginLeft: "8px" }}>
+            {new Date(review.createdAt).toLocaleString("ru-RU", options)}
+          </span>
         </div>
       </Card.Header>
       <Card.Body>
